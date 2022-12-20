b0ebdd420868029aae1095879c21064b7383b49c
1a04i1 <119719040+1a04i1@users.noreply.github.com>
2022-12-20 19:41:53 +0530

    #include <stdio.h> int main() {     int i,n,j;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=i;j++)         {             printf("%c ",'A'-1 + i);         }         printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,16 @@
-#include<stdio.h>
+#include <stdio.h> 
 int main()
 {
     int i,n,j;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=n;i>=1;i--)
     {
         for(j=1;j<=i;j++)
         {
-            printf("%d",j);
+            printf("%c ",'A'-1 + i);
         }
-    printf("
+        printf("
 ");
     }
+    return 0;
 }
