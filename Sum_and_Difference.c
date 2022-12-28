9b99fb1be0bc2de44ba818003c37a922e85df367
1a04i1 <119719040+1a04i1@users.noreply.github.com>
2022-12-28 22:43:58 +0530

    #include<stdio.h> int main() {     int a,b,s,m;     scanf("%d%d",&a,&b);     float c,d,add,diff;     scanf("%f%f",&c,&d);     s=a+b;     m=a-b;         printf("%d %d",s,m);         {             printf(" ");         }         add=c+d;         diff=c-d;         printf("%0.1f %0.1f",add,diff);         {             printf(" ");         } }

---
+++ Sum_and_Difference.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,s,m;
+    scanf("%d%d",&a,&b);
+    float c,d,add,diff;
+    scanf("%f%f",&c,&d);
+    s=a+b;
+    m=a-b;
+        printf("%d %d",s,m);
+        {
+            printf("
+");
+        }
+        add=c+d;
+        diff=c-d;
+        printf("%0.1f %0.1f",add,diff);
+        {
+            printf("
+");
+        }
+}
