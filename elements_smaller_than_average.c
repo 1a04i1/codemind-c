a85a58e50cee77f24a1c84734114e50b309e4ad9
1a04i1 <119719040+1a04i1@users.noreply.github.com>
2022-12-29 10:50:57 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n],s=0,avg,c=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);         s=s+a[i];     }     avg=s/n;     for(i=0;i<n;i++)     {         if(avg>=a[i])         {             c++;         }     }     printf("%d",c); }

---
+++ elements_smaller_than_average.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int a[n],s=0,avg,c=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        s=s+a[i];
+    }
+    avg=s/n;
+    for(i=0;i<n;i++)
+    {
+        if(avg>=a[i])
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
