cbc0fb2c2883105fceb8275ab6ffabe693b77b0d
1a04i1 <119719040+1a04i1@users.noreply.github.com>
2022-12-30 10:34:59 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     while(n--)     {         int a,b,c,d;         scanf("%d%d",&a,&b);         c=a*5;         d=b*7;         printf("%d ",c+d);     } }

---
+++ Tour.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while(n--)
+    {
+        int a,b,c,d;
+        scanf("%d%d",&a,&b);
+        c=a*5;
+        d=b*7;
+        printf("%d
+",c+d);
+    }
+}
